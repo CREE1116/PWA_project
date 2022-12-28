ad7c49ac95a8b3690e6b0c6781fcf9faf7720a51
leejongmin <60211689@mju.ac.kr>
2022-12-28 17:50:32 +0900

    13

--- src/routes/GenreHome.jsx
+++ src/routes/GenreHome.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import Page from "../components/Page";
 function GenreHome(props) {
   let params = useParams();
+  console.log(params);
   const fetchUrl = `https://api.jikan.moe/v4/anime/?genres=${params.genreId}`;
   return (
     <div>
